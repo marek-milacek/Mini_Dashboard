1d84b891cee628aed262de5bab158d5002353648
marek-milacek <marek.milacek@outlook.cz>
2026-04-01 16:56:46 +0200

    render update back

--- db/pool.js
+++ db/pool.js
@@ -3,9 +3,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl:
-        process.env.NODE_ENV === "production"
-            ? { rejectUnauthorized: false }
-            : false,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
